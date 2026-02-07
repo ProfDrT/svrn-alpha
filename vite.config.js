40541a73be285a715bae267cc4ea440a4fb34564
ProfDrT <tobiasblask@gmail.com>
2026-02-07 23:36:53 +0100

    chore: make base path conditional for cross-platform deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: "/svrn-alpha/",
+    base: process.env.GITHUB_PAGES ? "/svrn-alpha/" : "/",
 })
